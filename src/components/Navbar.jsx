e38287a8576ee6b256a4f780a7194b0e4b831aae
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-11-24 21:39:05 +0600

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,28 +5,66 @@ export default function Navbar() {
     const links = (
         <>
             <li>
-                <Link className="hover:text-[#FF8A00]" href="">Read Stories</Link>
+                <Link className="" href="">
+                    Read Stories
+                </Link>
             </li>
             <li>
-                <Link className="hover:text-[#FF8A00]" href="">Add Story</Link>
+                <Link className="" href="">
+                    Add Story
+                </Link>
             </li>
             <li>
-                <Link className="hover:text-[#FF8A00]" href="">Manage Stories</Link>
+                <Link className="" href="">
+                    Manage Stories
+                </Link>
             </li>
         </>
     );
     return (
-        <div className="bg-[#FFFFFF] sticky border-b border-[#D8D6CF]">
-            <nav className="flex justify-between max-w-7xl mx-auto p-2 font-semibold">
-                <div>StoryTime</div>
-                <ul className="flex gap-3">{links}</ul>
-                <div>
-                    <div className="flex gap-3">
-                        <Link href="">Login</Link>
-                        <Link href="">Register</Link>
+        <div className="bg-base-100 shadow-sm">
+            <div className="max-w-7xl mx-auto navbar">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost lg:hidden"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                {" "}
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />{" "}
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                        >
+                            {links}
+                        </ul>
                     </div>
+                    <Link href="/">
+                        <img className="w-12" src="storiesStack.png" alt="" />
+                    </Link>
                 </div>
-            </nav>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">{links}</ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
+            </div>
         </div>
     );
 }
