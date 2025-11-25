415554ef3d03eeada713c3c1197f9e1f7b9e05d7
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-11-25 18:28:28 +0600

    added Quote of the day section

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner";
+import QuoteOfTheDay from "@/components/QuoteOfTheDay";
 import TopAuthors from "@/components/TopAuthors";
 import Image from "next/image";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="max-w-7xl mx-auto">
       <Banner />
       <TopAuthors />
+      <QuoteOfTheDay />
     </div>
   );
 }
---
+++ src/components/QuoteOfTheDay.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function QuoteOfTheDay() {
+  return (
+    <div className='mt-10 bg-base-200'>
+        <div className='max-w-7xl mx-auto p-2'>
+            <h1 className='font-bold text-3xl mb-5'>Quote of the Day</h1>
+            <div className='rounded-md bg-secondary p-7 w-3/5 mx-auto text-center'>
+                <p className='text-3xl text-base-100 font-semibold'>
+                    "A reader lives a thousand lives before he dies."
+                </p>
+                <p className='mt-3 text-right font-semibold'>- George R. R. Martin</p>
+            </div>
+        </div>
+    </div>
+  )
+}
